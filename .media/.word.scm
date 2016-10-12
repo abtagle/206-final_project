dec0614fbcc1b2a5d55d18d3a2ec4603265569a0
atag549 <atag549@xen-rlimage.ece.auckland.ac.nz>
2016-10-13 10:23:26 +1300

    Fixed a small bug in ButtonSound where the sound randomly doesn't play

--- .media/.word.scm
+++ .media/.word.scm
@@ -1,2 +1,2 @@
 (voice_kal_diphone) ;;
-(SayText "friendly")
+(SayText "rich")
--- src/voxSpell/quiz/ButtonSound.java
+++ src/voxSpell/quiz/ButtonSound.java
@@ -5,6 +5,7 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
 import javax.swing.SwingWorker;
 
 
@@ -20,6 +21,8 @@ public class ButtonSound extends SwingWorker<Void, Void>{
 		AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File(Lists.PATH+SOUND+_soundName).getAbsoluteFile());
 		Clip clip = AudioSystem.getClip();
 		clip.open(audioInputStream);
+		FloatControl gainControl = (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+		gainControl.setValue(-15.0f);
 		clip.start();
 		return null;
 	}
