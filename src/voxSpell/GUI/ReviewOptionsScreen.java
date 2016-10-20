6a44698846f1a4b6ae1335b7fbe765a9763af405
atag549 <atag549@xen-rlimage.ece.auckland.ac.nz>
2016-10-21 10:30:02 +1300

    Added help button to the review options screen

--- src/voxSpell/GUI/ReviewOptionsScreen.java
+++ src/voxSpell/GUI/ReviewOptionsScreen.java
@@ -7,9 +7,16 @@ import java.awt.GridBagConstraints;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import java.awt.Insets;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.ActionEvent;
+import javax.swing.UIManager;
+
+import voxSpell.help.HelpScreen;
 
 public class ReviewOptionsScreen extends JPanel {
 
@@ -26,6 +33,7 @@ public class ReviewOptionsScreen extends JPanel {
 		gridBagLayout.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		setLayout(gridBagLayout);
 		
+		//Review Title
 		JLabel title = new JLabel("Review");
 		title.setVerticalAlignment(SwingConstants.BOTTOM);
 		title.setHorizontalAlignment(SwingConstants.CENTER);
@@ -38,6 +46,7 @@ public class ReviewOptionsScreen extends JPanel {
 		gbc_title.gridy = 1;
 		add(title, gbc_title);
 		
+		//Button for failed review
 		JButton reviewFailed = new JButton("Review Failed Words");
 		reviewFailed.setBackground(GUI.background);
 		reviewFailed.addActionListener(new ActionListener() {
@@ -55,6 +64,7 @@ public class ReviewOptionsScreen extends JPanel {
 		gbc_reviewFailed.gridy = 3;
 		add(reviewFailed, gbc_reviewFailed);
 		
+		//Button for current level review
 		JButton btnReviewCurrentList = new JButton("Review Current Level");
 		btnReviewCurrentList.setBackground(GUI.background);
 		btnReviewCurrentList.addActionListener(new ActionListener() {
@@ -72,21 +82,39 @@ public class ReviewOptionsScreen extends JPanel {
 		gbc_btnReviewCurrentList.gridy = 5;
 		add(btnReviewCurrentList, gbc_btnReviewCurrentList);
 		
-		JButton button = new JButton("Back To Menu");
-		button.setBackground(GUI.background);
-		button.addActionListener(new ActionListener() {
+		//back to menu button
+		JButton menu = new JButton("Back To Menu");
+		menu.setBackground(GUI.background);
+		menu.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				GUI.getInstance().setContentPane(new MainMenu());
 			}
 		});
-		button.setForeground(GUI.foreground);
-		button.setFont(new Font(GUI.FONT, Font.PLAIN, 28));
-		GridBagConstraints gbc_button = new GridBagConstraints();
-		gbc_button.fill = GridBagConstraints.HORIZONTAL;
-		gbc_button.insets = new Insets(0, 0, 5, 5);
-		gbc_button.gridx = 1;
-		gbc_button.gridy = 7;
-		add(button, gbc_button);
+		menu.setForeground(GUI.foreground);
+		menu.setFont(new Font(GUI.FONT, Font.PLAIN, 28));
+		GridBagConstraints gbc_menu = new GridBagConstraints();
+		gbc_menu.fill = GridBagConstraints.HORIZONTAL;
+		gbc_menu.insets = new Insets(0, 0, 5, 5);
+		gbc_menu.gridx = 1;
+		gbc_menu.gridy = 7;
+		add(menu, gbc_menu);
+		
+		//help button
+		JLabel help = new JLabel("");
+		help.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				new HelpScreen();
+			}
+		});
+		help.setIcon(new ImageIcon(GUI.PATH + "/help.png"));
+		help.setHorizontalAlignment(SwingConstants.RIGHT);
+		help.setFont(new Font(GUI.FONT, Font.PLAIN, 28));
+		help.setBackground(GUI.background);
+		GridBagConstraints gbc_help = new GridBagConstraints();
+		gbc_help.insets = new Insets(0, 0, 5, 0);
+		gbc_help.gridx = 4;
+		gbc_help.gridy = 7;
+		add(help, gbc_help);
 
 	}
 
